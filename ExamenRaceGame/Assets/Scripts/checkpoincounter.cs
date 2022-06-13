24160d818bc6f63564e8505d45ef3c150c896def
AlexanderMertens2002 <r0842789@ucll.be>
2022-06-13 16:35:04 +0200

    checkpoints

---
+++ ExamenRaceGame/Assets/Scripts/checkpoincounter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoincounter : MonoBehaviour
+{
+    public float checkpoint = 0f;
+    
+
+    void OnTriggerEnter(Collider other) {
+        checkpoint = 1f;
+    }
+}
